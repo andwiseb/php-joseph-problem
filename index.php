12c6379d85facfa7b4a9d19a09e7a0f16e079b15
andwise <34187275+andwiseb@users.noreply.github.com>
2021-04-05 17:44:19 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,16 @@ echo
 how many players?: <input type='text' name='player_numbers' autocomplete='off'><br><br>
 <input type='submit' name='send' value='play and die!'>
 </form>";	
-
 if ($_POST) {
 	$number = $_POST['player_numbers'];
 	$set = array();
 	for ($i = 0; $i < $number; $i++) { 
 		$set[$i] = $i + 1;
 	}
-
 	$say = count($set) - 1;
 	for ($i = 1; $i < $number; $i = $i + 2) { 
 		unset($set[$i]);
 	} 
-
 	while (count($set) != 1) {
 		if (empty($set[$say])) {
 			$say = count($set) - 1;
@@ -33,11 +30,7 @@ if ($_POST) {
 				unset($set[$i]);
 			}
 		}
-
 	}
-
 	$set = array_values($set);
 	echo "<p class='winner'><b>winner:</b> number $set[0]</p>";
 }
-
-	?>
