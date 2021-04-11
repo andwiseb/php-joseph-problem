6440212d63e9ab5272ad4183c8469a5a357f9713
andwise <34187275+andwiseb@users.noreply.github.com>
2021-04-11 13:51:14 +0300

    Update play.php

--- play.php
+++ play.php
@@ -1,30 +1,31 @@
-<?php
+<form action="" method="POST">
+    how many players?:
+    <input type="text" name="player_numbers" autocomplete="off"><br><br>
+    <input type="submit" name="send" value="play and die!">
+</form>
 
-echo
-"<form name='joseph_form' action='' method='POST'>
-how many players?: <input type='text' name='player_numbers' autocomplete='off'><br><br>
-<input type='submit' name='send' value='play and die!'>
-</form>";	
+<?php
+	
 if ($_POST) {
 	$number = $_POST['player_numbers'];
 	$set = array();
 	for ($i = 0; $i < $number; $i++) { 
 		$set[$i] = $i + 1;
 	}
-	$say = count($set) - 1;
+	$count = count($set) - 1;
 	for ($i = 1; $i < $number; $i = $i + 2) { 
 		unset($set[$i]);
 	} 
 	while (count($set) != 1) {
-		if (empty($set[$say])) {
-			$say = count($set) - 1;
+		if (empty($set[$count])) {
+			$count = count($set) - 1;
 			$set = array_values($set);
 			for ($i = 1; $i < $number; $i = $i + 2) { 
 				unset($set[$i]);
 			}
 		}
 		else {
-			$say = count($set) - 1;
+			$count = count($set) - 1;
 			$set = array_values($set);
 			for ($i = 0; $i < $number; $i = $i + 2) { 
 				unset($set[$i]);
